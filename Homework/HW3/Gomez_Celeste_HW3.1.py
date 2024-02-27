0015f513642c4d79b22cc033981dc54089802cac
cgooooo <157634662+cgooooo@users.noreply.github.com>
2024-02-27 14:33:15 -0800

    Add files via upload

---
+++ Homework/HW3/Gomez_Celeste_HW3.1.py
@@ -0,0 +1,12 @@
+#Power of a Number 
+#exponents is just multiplying x by x, y number of times
+
+x=5
+y=2
+num = 1
+def expo():
+    return (num)
+
+for i in range(y): #loop from 0 to y 
+    num = num * x
+    print({num})
